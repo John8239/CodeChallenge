540ad67985624fc41f7f819a91c4cd523719d5da
John S <johnms8239@gmail.com>
2025-01-01 12:59:08 -0500

    Added ReportingStructureController with a shell of the Get funciton

---
+++ CodeChallenge/Controllers/ReportingStructureController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using CodeChallenge.Services;
+using CodeChallenge.Models;
+
+namespace CodeChallenge.Controllers
+{
+    [ApiController]
+    [Route("api/reportingStructure")]
+    public class ReportingStructureController : ControllerBase
+    {
+        private readonly ILogger _logger;
+        private readonly IEmployeeService _employeeService;
+
+        public ReportingStructureController(ILogger<EmployeeController> logger, IEmployeeService employeeService)
+        {
+            _logger = logger;
+            _employeeService = employeeService;
+        }
+
+        [HttpGet("{id}", Name = "getReportingStructureByEmployeeId")]
+        public IActionResult GetReportingStructureByEmployeeId(String id)
+        {
+            _logger.LogDebug($"Received ReportingStructure get request for EmployeeId '{id}'");
+
+
+
+            return Ok();
+        }
+    }
+}
