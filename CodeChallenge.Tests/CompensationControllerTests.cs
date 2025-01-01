7a0f863ab02bd366ec3b2a8463144eea3aa8ff9f
John S <johnms8239@gmail.com>
2025-01-01 15:11:17 -0500

    CompensationController Get Tests

---
+++ CodeChallenge.Tests/CompensationControllerTests.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+
+using CodeChallenge.Models;
+
+using CodeCodeChallenge.Tests.Integration.Extensions;
+using CodeCodeChallenge.Tests.Integration.Helpers;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace CodeChallenge.Tests.Integration
+{
+    [TestClass]
+    public class CompensationControllerTests
+    {
+        private static HttpClient _httpClient;
+        private static TestServer _testServer;
+
+        [ClassInitialize]
+        // Attribute ClassInitialize requires this signature
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "<Pending>")]
+        public static void InitializeClass(TestContext context)
+        {
+            _testServer = new TestServer();
+            _httpClient = _testServer.NewClient();
+        }
+
+        [ClassCleanup]
+        public static void CleanUpTest()
+        {
+            _httpClient.Dispose();
+            _testServer.Dispose();
+        }
+
+        [TestMethod]
+        public void GetCompensationByEmployeeId_Returns_Ok()
+        {
+            // Arrange
+            var employeeId = "16a596ae-edd3-4847-99fe-c4518e82c86f";
+            var expectedFirstName = "John";
+            var expectedLastName = "Lennon";
+            var expectedSalary = 1000000.00;
+
+            // Execute
+            var getRequestTask = _httpClient.GetAsync($"api/compensation/{employeeId}");
+            var response = getRequestTask.Result;
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            var compensation = response.DeserializeContent<Compensation>();
+            Assert.IsNotNull(compensation);
+            Assert.IsNotNull(compensation.Employee);
+            Assert.IsNotNull(compensation.CompensationId);
+            Assert.IsNotNull(compensation.EffectiveDate);
+            Assert.AreEqual(expectedFirstName, compensation.Employee.FirstName);
+            Assert.AreEqual(expectedLastName, compensation.Employee.LastName);
+            Assert.AreEqual(expectedSalary, compensation.Salary);
+        }
+
+        [TestMethod]
+        public void GetCompensationByEmployeeId_Returns_NotFound()
+        {
+            // Arrange
+            var employeeId = "FakeEmployeeId";
+
+            // Execute
+            var getRequestTask = _httpClient.GetAsync($"api/compensation/{employeeId}");
+            var response = getRequestTask.Result;
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.NotFound, response.StatusCode);
+        }
+    }
+}
